d473c18179873f7f7463aef32e42db0015c39cd0
kampanita <kampanita@gmail.com>
2015-09-08 16:36:28 +0200

    Update peelink.py
    No esta del todo fino, pero va

--- python/main-classic/channels/peelink.py
+++ python/main-classic/channels/peelink.py
@@ -3,9 +3,8 @@
 # pelisalacarta - XBMC Plugin
 # Canal para peelink - Por Kampanita-2015 
 # ( con ayuda de neno1978, DrZ3r0, y robalo )
-# 7/9/2015
+# 4/9/2015
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
-# De momento solo listo por categorías
 #-----------------------------------------------------------------
 import urlparse,urllib2,urllib,re
 import os, sys
@@ -32,6 +31,8 @@ def isGeneric():
 def mainlist(item):
     logger.info("[peelink] mainlist")
     itemlist = []
+    itemlist.append( Item(channel=__channel__, action="menupelis", title="Peliculas",  url="http://www.peelink2.org" , thumbnail="http://primerasnoticias.com/wp-content/uploads/2012/07/game1.jpg", fanart="http://primerasnoticias.com/wp-content/uploads/2012/07/game1.jpg") )
+    itemlist.append( Item(channel=__channel__, action="ultimas", title="Ultimas",  url="http://www.peelink2.org" , thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDsZyDowjAAE23njJbp9hYZRe9viAuq-f1niz2nRC4jNwXkD6W", fanart="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDsZyDowjAAE23njJbp9hYZRe9viAuq-f1niz2nRC4jNwXkD6W") )
     itemlist.append( Item(channel=__channel__, action="porcat", title="Por Categoria",  url="http://www.peelink2.org/p/indice-de-pelis.html" , thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDsZyDowjAAE23njJbp9hYZRe9viAuq-f1niz2nRC4jNwXkD6W", fanart="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDsZyDowjAAE23njJbp9hYZRe9viAuq-f1niz2nRC4jNwXkD6W") )
     itemlist.append( Item(channel=__channel__, action="search", title="Buscar...", url="http://www.peelink2.org/search/?s=", thumbnail="http://thumbs.dreamstime.com/x/buscar-pistas-13159747.jpg", fanart="http://thumbs.dreamstime.com/x/buscar-pistas-13159747.jpg"))
     return itemlist
@@ -53,6 +54,113 @@ def porcat(item):
 
     return itemlist
 
+def menupelis(item):
+    logger.info("[peelink] menupelis")
+    logger.info("[peelink] :"+item.url)
+    
+    itemlist = []
+        
+    data = scrapertools.cache_page(item.url).decode('iso-8859-1').encode('utf-8')          
+    
+    patronbloque = '<div.*?<p><a(.*?)</a></p>'         #patron para principal_con_todo--> se salta el primer enlace    
+    matchesbloque = re.compile(patronbloque,re.DOTALL).findall(data)    
+    
+    for datos in matchesbloque:
+
+        patron= 'href="([^"]+)"><img.*?src="([^"]+)"'
+        matches = re.compile(patron,re.DOTALL).findall(datos)    
+
+        for scrapedurl,scrapedthumbnail in matches:
+            url = urlparse.urljoin(item.url,scrapedurl)               
+            thumbnail=urlparse.urljoin(item.thumbnail,scrapedthumbnail)   
+           
+            patron='.*?//.*?/\d+/\d+/(.*?).jpg'
+            matches = re.compile(patron,re.DOTALL).findall(thumbnail)    
+
+            for dato in matches:
+                title=dato.replace("Ver","")
+                title=title.replace("ver","")
+                title=title.replace("-n","")
+                title=title.replace("-"," ")
+                
+            itemlist.append( Item(channel=__channel__, action="verpeli", title=title, fulltitle=title , url=url, thumbnail=thumbnail) )
+    
+        patron = '<link rel="canonical" href="([^"]+)"'
+        matches = re.compile(patron,re.DOTALL).findall(data)    
+
+        for scrapedurl in matches:
+            url = urlparse.urljoin(item.url,scrapedurl)                                   
+            pagina_actual = url
+
+    #puta_paginacion
+    try:
+                
+        patron='<a href="([^"]+-estrenos.html)">'
+        bloquematches = re.compile(patron,re.DOTALL).findall(data)    
+
+        for scrapedurl in bloquematches:
+            url = urlparse.urljoin(item.url,scrapedurl)             
+            pagina_actual = pagina_actual.replace("www.","")             
+            if url == pagina_actual:
+               try:
+                  dato = scrapertools.get_match(pagina_actual,'pagina-(\d+)-estrenos')               
+                  break
+               except:        
+                  dato = "1"                                                  
+            else:        
+               dato_busq = "1"
+        dato_busq = str (int(dato) + 1 )                               
+        
+        patron='</a>.*?<a href="([^"]+-estrenos.html)">'
+        bloquematches = re.compile(patron,re.DOTALL).findall(data)    
+         
+        for scrapedurl in bloquematches:                        
+               url = urlparse.urljoin(item.url,scrapedurl)               
+               dato2 = scrapertools.get_match(url,'pagina-(\d+)-estrenos')                              
+               
+               if dato_busq == dato2:                 
+                  itemlist.append( Item(channel=__channel__, title="Pagina [COLOR red]"+dato2+"[/COLOR]", url=url, action="menupelis",  folder=True) )                 
+                  break               
+        
+    except: pass
+    
+    return itemlist 		
+    
+    
+def ultimas(item):
+    logger.info("[peelink] menupelis")
+    logger.info("[peelink] "+item.url)
+    
+    itemlist = []
+    
+    itemlist.append( Item(channel=__channel__, title="Pagina actual ( "+item.url+" )",  folder=True) )
+        
+    data = scrapertools.cache_page(item.url).decode('iso-8859-1').encode('utf-8')          
+    
+    patronbloque = '<div class=.*?<p>(.*?)</a></p>'         #patron para principal_con_todo
+
+    matchesbloque = re.compile(patronbloque,re.DOTALL).findall(data)    
+    
+    scrapertools.printMatches(matchesbloque)
+    
+    for datos in matchesbloque:
+        patron= 'href="([^"]+)"><img.*?src="([^"]+)".*?alt="([^"]+)".*?</a>'        
+        matches = re.compile(patron,re.DOTALL).findall(datos)    
+        
+        scrapertools.printMatches(matches)
+    
+        for scrapedurl,scrapedthumbnail,scrapedtitle in matches:        
+            title = scrapedtitle.replace("Ver","")              
+            title = title.replace("ver","")                          
+            url = urlparse.urljoin(item.url,scrapedurl)               
+            thumbnail=urlparse.urljoin(item.thumbnail,scrapedthumbnail)   
+            logger.info("THUMBNAIL : -------------------- "+thumbnail)
+            
+            itemlist.append( Item(channel=__channel__, action="verpeli", title=title, fulltitle=title , url=url, thumbnail=thumbnail) )
+    
+    return itemlist 		
+    
+    
 def menucat(item):
    
     logger.info("[peelink] menupecat")
@@ -77,15 +185,11 @@ def menucat(item):
             url = urlparse.urljoin(item.url,scrapedurl)   
             logger.info("[peelink]  title: " + title + " url: " + url )         
             itemlist.append( Item(channel=__channel__, action="verpeli", title=title, fulltitle=title , url=url) )
-        
-        
-        try:
-            next_page = scrapertools.get_match(data,'<span style="font-size: small;">\d+ DE \d+</span>.*?<a href="[^"]+".*?<a href="([^"]+)"')            
-            title= "[COLOR red][B]Pagina siguiente »[/B][/COLOR]"
-            itemlist.append( Item(channel=__channel__, title=title, url=next_page, action="menupelis",  folder=True) )
-        except: pass
-        
-        return itemlist
+               
+   
+    #aqui no hay paginación
+    return itemlist 		
+    
         
 def verpeli(item):   
    
@@ -102,7 +206,7 @@ def verpeli(item):
    itemlist = []      
    for scrapedthumbnail,scrapedplot in matches:
        thumbnail=urlparse.urljoin(item.url,scrapedthumbnail)   
-       plot=scrapertools.htmlclean(scrapedplot)
+       plot=scrapertools.htmlclean(scrapedplot).decode('iso-8859-1').encode('utf-8')          
    #estos son los datos para plot 
    
    patron='<img class="alignnone.*?>(.*?)</span></div>'
@@ -114,7 +218,7 @@ def verpeli(item):
        match = re.compile(patron,re.DOTALL).findall(datos)            
        scrapertools.printMatches(match)
        for scrapedurl in match:
-           url = urlparse.urljoin(item.url,scrapedurl)            
+           url = urlparse.urljoin(item.url,scrapedurl.decode('iso-8859-1').encode('utf-8'))            
            surl = "[ [COLOR red]"+scrapertools.get_match(url,'.*?://(.*?)/')+"[/COLOR] ]" 
            itemlist.append( Item(channel=__channel__, action="play", fanart=thumbnail, thumbnail=thumbnail,title=item.title+" "+surl, fulltitle=item.title , url=url, plot=plot) )
    
