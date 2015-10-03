5fb02d88fc72819af55363addc7231641ef86a72
Jesus <tvalacarta@gmail.com>
2015-10-03 12:58:38 +0200

    Añadido logo newpct1

--- python/main-classic/channelselector.py
+++ python/main-classic/channelselector.py
@@ -264,6 +264,7 @@ def channels_list():
     if config.get_setting("enableadultmode") == "true": itemlist.append(Item( title="PornHub", channel="pornhub", language="ES" , category="X,F" , type="generic" ,thumbnail="http://s22.postimg.org/5lzcocfqp/pornhub_logo.jpg" )) # superberny 19/01/2015
     itemlist.append( Item( title="Quebajamos"              , channel="quebajamos"         , language="ES" , category="F,S,D" , type="generic"  )) # jesus 16/06/2014
     itemlist.append( Item( title="Quiero Dibujos Animados" , channel="quierodibujosanimados",language="ES", category="S" , type="generic"  )) # jesus 12/11/2012
+    #itemlist.append( Item( title="Repelis"                 , channel="repelis"            , language="ES" , category="F"          , type="generic"  ))
     itemlist.append( Item( title="Reyanime"                , channel="reyanime"           , language="ES" , category="A"          , type="generic"  ))
     itemlist.append( Item( title="Robinfilm (IT)"          , channel="robinfilm"          , language="IT" , category="F"          , type="generic"  )) # jesus 16/05/2011
     itemlist.append( Item( title="Seriesadicto"            , channel="seriesadicto"       , language="ES" , category="S,A"          , type="generic" ))
