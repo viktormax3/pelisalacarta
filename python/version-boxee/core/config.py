d358c1aed81a36b6c8c062abdcb36cb16f6605c7
Jesus <tvalacarta@gmail.com>
2015-10-04 08:31:11 +0200

    Cambios version Boxee

--- python/version-boxee/core/config.py
+++ python/version-boxee/core/config.py
@@ -11,8 +11,8 @@
 # Historial de cambios:
 #------------------------------------------------------------
 
-TAG_VERSION = "10.10"
-TAG_VERSION_XBMC = "3.9.99"
+TAG_VERSION = "10.12"
+TAG_VERSION_XBMC = "4.0.3"
 PLATFORM_NAME = "boxee"
 
 print "[config.py] boxee config "+TAG_VERSION+" ("+TAG_VERSION_XBMC+")"
