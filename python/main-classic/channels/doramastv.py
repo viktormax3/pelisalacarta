c8ae84b825dc51d8fc4528921918db91bf6780ed
raulhaag <raghaag@gmail.com>
2016-03-05 19:28:01 -0300

    update doramastv

--- python/main-classic/channels/doramastv.py
+++ python/main-classic/channels/doramastv.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #------------------------------------------------------------
 # pelisalacarta - XBMC Plugin
-# Canal para doramastv
+# Canal para seriesflv
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
 #------------------------------------------------------------
 
@@ -26,6 +26,7 @@ __creationdate__ = "20160216"
 
 host = "http://doramastv.com/"
 
+
 DEFAULT_HEADERS = []
 DEFAULT_HEADERS.append( ["User-Agent","Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; es-ES; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12"] )
 
@@ -34,11 +35,13 @@ def isGeneric():
 
 def mainlist(item):
     logger.info("pelisalacarta.channels.doramatv mainlist")
-    itemlist = []
+	
+    itemlist = list([])
     itemlist.append(Item(channel=__channel__, action="pagina_", title="En emision", url=urlparse.urljoin(host, "drama/emision")))
     itemlist.append(Item(channel=__channel__, action="letras", title="Listado alfabetico", url=urlparse.urljoin(host, "lista-numeros")))
     itemlist.append(Item(channel=__channel__, action="generos", title="Generos", url=urlparse.urljoin(host, "genero/accion")))
     itemlist.append(Item(channel=__channel__, action="pagina_", title="Ultimos agregados", url=urlparse.urljoin(host, "dramas/ultimos")))
+    itemlist.append(Item(channel=__channel__, action="search", title="Buscar", url=urlparse.urljoin(host, "buscar/anime/ajax/?title=")))
 
     return itemlist
 
@@ -63,7 +66,7 @@ def letras(item):
         itemlist.append(Item(channel=__channel__, action="pagina_", title=title, url=url, thumbnail=thumbnail, plot=plot))
 
     return itemlist
-
+	
 def pagina_(item):
     logger.info("pelisalacarta.channels.daramatv letras" + item.url)
     itemlist = []
@@ -76,10 +79,11 @@ def pagina_(item):
     matches = re.compile(patron, re.DOTALL).findall(data1)
     for scrapedurl, scrapedplot, scrapedthumbnail, scrapedtitle in matches:
         title = scrapertools.unescape(scrapedtitle).strip()
-        fulltitle = title
         url = urlparse.urljoin(item.url, scrapedurl)
         thumbnail = urlparse.urljoin(host, scrapedthumbnail)
-        itemlist.append( Item(channel=__channel__, action="episodios" , title=title , url=url, thumbnail=thumbnail, plot=scrapedplot, show=title))
+        plot = scrapertools.decodeHtmlentities(scrapedplot)
+        itemlist.append( Item(channel=__channel__, action="episodios" , title=title , url=url, thumbnail=thumbnail, plot=plot, show=title))
+	
     patron = 'href="([^"]+)" class="next"'
     matches = re.compile(patron, re.DOTALL).findall(data)
     for match in matches:
@@ -90,7 +94,7 @@ def pagina_(item):
             scrapedplot = ""
             itemlist.append(Item(channel=__channel__, action="pagina_", title=scrapedtitle, url=scrapedurl, thumbnail=scrapedthumbnail, plot=scrapedplot, folder=True))
     return itemlist
-
+	
 def episodios(item):
     logger.info("pelisalacarta.channels.doramatv episodios")
     itemlist = []
@@ -98,7 +102,7 @@ def episodios(item):
     data = scrapertools.cache_page(item.url,headers=headers)
     data = data.replace('\n','')
     data = data.replace('\r','')
-    data1 = scrapertools.get_match(data, 'apitulos -->(.+?)<!-- Fin de Lista de Capitulos')
+    data1 = scrapertools.get_match(data, '<ul id="lcholder">(.+?)</ul>')
     patron  = '<a href="(.+?)".+?>(.+?)<'
     matches = re.compile(patron,re.DOTALL).findall(data1)
     
@@ -110,7 +114,7 @@ def episodios(item):
         show = item.show
         itemlist.append( Item(channel=__channel__, action="findvideos" , title=title , url=url, thumbnail=thumbnail, plot=plot, fulltitle=title, show=show))		
     return itemlist
-
+	
 def findvideos(item):
     logger.info("pelisalacarta.channels.doramatv findvideos")
 
@@ -122,10 +126,9 @@ def findvideos(item):
     matches = re.compile(patron, re.DOTALL).findall(data)
     data1 = ''
     for match in matches:
-        data1 += match + '\n'
+		data1 += match + '\n'
     data = data1
     data = data.replace('%26','&')
-    data = data.replace('http://ozhe.larata.in/repro-d/vk?v=','http://vk.com/video_ext.php?oid=')
     data = data.replace('http://ozhe.larata.in/repro-d/mvk?v=','http://vk.com/video_ext.php?oid=')
     data = data.replace('http://ozhe.larata.in/repro-d/send?v=','http://sendvid.com/embed/')
     data = data.replace('http://ozhe.larata.in/repro-d/msend?v=','http://sendvid.com/embed/')
@@ -133,6 +136,7 @@ def findvideos(item):
     data = data.replace('http://ozhe.larata.in/repro-d/nowv?v=','http://www.nowvideo.sx/video/')
     data = data.replace('http://ozhe.larata.in/repro-d/nov?v=','http://www.novamov.com/video/')
     itemlist = []
+
     from servers import servertools
     itemlist.extend(servertools.find_video_items(data=data))
     for videoitem in itemlist:
@@ -147,10 +151,8 @@ def generos(item):
     data = scrapertools.cache_page(item.url,headers=headers)
     data = data.replace('\n','')
     data = data.replace('\r','')
+
     data = scrapertools.get_match(data,'<!-- Lista de Generos -->(.+?)<\/div>')
-    f = open( 'C:/Users/Raul/AppData/Roaming/Kodi/addons/plugin.video.pelisalacarta/channels/dor.txt', 'w' )
-    f.write( 'dict = ' + repr(dict) + '\n' )
-    f.close()
     patron = '<a href="(.+?)".+?>(.+?)<'
     matches = re.compile(patron, re.DOTALL).findall(data)
     for scrapedurl, scrapedtitle in matches:
@@ -164,3 +166,21 @@ def generos(item):
         itemlist.append(Item(channel=__channel__, action="pagina_", title=title, url=url, thumbnail=thumbnail, plot=plot))
 
     return itemlist
+	
+def search(item, texto):
+    logger.info("pelisalacarta.channels.doramatv search")
+    item.url = urlparse.urljoin(host, item.url)
+    texto = texto.replace(" ", "+")
+    headers = DEFAULT_HEADERS[:]
+    data = scrapertools.cache_page(item.url + texto,headers=headers)
+    data = data.replace('\n','')
+    data = data.replace('\r','')
+    patron = '<a href="(.+?)".+?src="(.+?)".+?titulo">(.+?)<'
+    matches = re.compile(patron, re.DOTALL).findall(data)
+    itemlist = []
+    for scrapedurl, scrapedthumbnail, scrapedtitle in matches:
+        title = scrapertools.unescape(scrapedtitle).strip()
+        url = urlparse.urljoin(item.url, scrapedurl)
+        thumbnail = urlparse.urljoin(host, scrapedthumbnail)
+        itemlist.append( Item(channel=__channel__, action="episodios" , title=title , url=url, thumbnail=thumbnail, plot="", show=title))
+	return itemlist
