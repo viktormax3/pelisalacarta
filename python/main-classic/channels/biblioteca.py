739712bb85770f99b74a5a5e29e4cabe7a978580
SeiTaN <seitan80@gmail.com>
2016-04-10 02:32:23 +0200

    Update biblioteca.py
    - añadido parte del menú.
    - muestra las series(carpetas) y capitulos (strm).
    
    pendiente de mostrar la info correctamente, para eso habrá que añadir las series con .nfo y posteriormente leerlo, en el caso que no exista .nfo, usar la api de super_berny.

--- python/main-classic/channels/biblioteca.py
+++ python/main-classic/channels/biblioteca.py
@@ -21,11 +21,21 @@ def mainlist(item):
     logger.info("pelisalacarta.channels.biblioteca mainlist")
    
     itemlist = list()
-    itemlist.append(Item(channel=__channel__, action="biblioteca", title="Biblioteca"))
+    itemlist.append(Item(channel=__channel__, action="peliculas", title="Películas"))
+    itemlist.append(Item(channel=__channel__, action="series", title="Series"))
     if config.get_library_support():
-        itemlist.append(Item(channel=__channel__, action="mainlist", title="Kodi", folder=False))
-        itemlist.append(Item(channel=__channel__, action="mainlist", title="      Películas", folder=False))
+        itemlist.append(Item(channel=__channel__, action="mainlist", title=""))
+        plot = "contenido agregado a la biblioteca de Kodi"
+        itemlist.append(Item(channel=__channel__, action="mainlist", title="Kodi", plot=plot))
+        itemlist.append(Item(channel=__channel__, action="kodi_pelis", title="      Películas"))
         itemlist.append(Item(channel=__channel__, action="kodi_series", title="      Series"))
+
+    return itemlist
+
+
+def peliculas(item):
+    logger.info("pelisalacarta.channels.biblioteca peliculas")
+    itemlist = []
 
     return itemlist
 
@@ -33,5 +43,16 @@ def mainlist(item):
 def kodi_series(item):
     logger.info("pelisalacarta.channels.biblioteca kodi_series")
     itemlist = []
+
+    from platformcode import library2
+    if hasattr(item, "element"):
+        new_item = item
+    else:
+        new_item = item.clone(element=1)
+
+    series_name = library2.get_tvshows_list(new_item)
+
+    for serie in series_name:
+        itemlist.append(Item(channel=__channel__, action="kodi_series", title=serie, element=2, path=serie))
 
     return itemlist
