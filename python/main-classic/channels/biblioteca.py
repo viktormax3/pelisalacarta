10ebc0afafc41e16e300ba2c46e34274c8bf77e0
superberny <superberny@gmail.com>
2016-05-15 12:55:26 +0200

    añadida gestion de series multicanal

--- python/main-classic/channels/biblioteca.py
+++ python/main-classic/channels/biblioteca.py
@@ -6,6 +6,7 @@
 # ------------------------------------------------------------
 
 import os
+import re
 
 from core import config
 from core import jsontools
@@ -29,7 +30,7 @@ def mainlist(item):
     itemlist = list()
     itemlist.append(Item(channel=__channel__, action="peliculas", title="Películas"))
     itemlist.append(Item(channel=__channel__, action="series", title="Series"))
-    itemlist.append(Item(channel=__channel__, action="fichero_series", title="Fichero de series"))
+    #itemlist.append(Item(channel=__channel__, action="fichero_series", title="Fichero de series"))
 
     return itemlist
 
@@ -86,7 +87,7 @@ def series(item):
     # Crear un item en la lista para cada carpeta encontrada
     for i in carpetas_series:
         path = library.join_path(raiz, i)
-        new_item = Item(channel=item.channel, action='get_temporadas', title=i, path=path)
+        new_item = Item(channel=item.channel, action ='get_temporadas', title=i, path=path)
         itemlist.append(new_item)
 
     library.set_infoLabels_from_library(itemlist, tipo='TVShows')
@@ -99,32 +100,60 @@ def get_temporadas(item):
     itemlist = list()
     dict_temp ={}
 
-    # Obtenemos los archivos de los capitulos
+    # Obtenemos las carpetas de los canales y los archivos de los capitulos
     if not samba.usingsamba(item.path):
         raiz, carpetas_series, ficheros = os.walk(item.path).next()
     else:
         raiz = item.path
+        carpetas_series = samba.get_directories(path)
         ficheros = samba.get_files(item.path)
 
-    # Obtenemos las temporadas del listado de strm
-    for i in ficheros:
-        if i.endswith('.strm'):
-            season = i.split('x')[0]
-            dict_temp[season] = "Temporada " + str(season)
+    if len(carpetas_series) > 1:
+        # Si hay varios canales...
+        for c in carpetas_series:
+            # ...creamos un item por cada canal
+            path = library.join_path(raiz, c)
+            logger.debug(item.tostring('\n'))
+            title = c
+            canal = re.search('\[(.*?)\]',c)
+            if canal:
+                canal = canal.group(1).capitalize()
+                title = "{0} [{1}]".format(item.title,canal)
+            new_item = item.clone(action='get_temporadas', title=title, path=path)
+            itemlist.append(new_item)
 
-    # Creamos un item por cada temporada
-    for k, v in dict_temp.items():
-        new_item = item.clone(action='get_capitulos', title = v, contentSeason = k, contentEpisodeNumber="")
-        itemlist.append(new_item)
+    elif len(carpetas_series) == 1:
+        # ...si solo hay un canal...
+        # ...obtenemos las temporadas de manera recursiva
+        item.path = library.join_path(raiz, carpetas_series[0])
+        return get_temporadas(item)
+    else:
+        # ...si ya estamos dentro del canal...
+        # ...obtenemos las temporadas del listado de strm
+        for i in ficheros:
+            if i.endswith('.strm'):
+                season = i.split('x')[0]
+                dict_temp[season] = "Temporada " + str(season)
 
-    return sorted(itemlist, key=lambda it: it.title)
+        # Creamos un item por cada temporada
+        for k, v in dict_temp.items():
+            new_item = item.clone(action='get_capitulos', title = v, contentSeason = k, contentEpisodeNumber="",
+                                  filtrar_season = True)
+            itemlist.append(new_item)
+            logger.debug(new_item.tostring())
 
+        if len(itemlist) > 1:
+            itemlist = sorted(itemlist, key=lambda it: int(it.contentSeason))
+            new_item = item.clone(action='get_capitulos', title ="*Todas las temporadas")
+            itemlist.insert(0,new_item)
+
+    return itemlist
 
 
 def get_capitulos(item):
     logger.info("pelisalacarta.channels.biblioteca get_capitulos")
     itemlist = list()
-
+    logger.debug(item.tostring('\n'))
     # Obtenemos los archivos de los capitulos
     if not samba.usingsamba(item.path):
         raiz, carpetas_series, ficheros = os.walk(item.path).next()
@@ -136,9 +165,9 @@ def get_capitulos(item):
     for i in ficheros:
         if i.endswith('.strm'):
 
-            if item.contentSeason and not i.startswith(item.contentSeason):
+            # Si hay q filtrar por temporada, ignoramos los capitulos de otras temporadas
+            if item.filtrar_season and int(i.split('x')[0]) != int(item.contentSeason):
                 continue
-                #print item.contentSeason, i, i.startswith(item.contentSeason)
 
             path = library.join_path(raiz, i)
             if not samba.usingsamba(raiz):
@@ -166,14 +195,15 @@ def play_strm(item):
     return itemlist
 
 
-def get_sort_temp_epi(item):
+def get_sort_temp_epi(item): # TODO SEitan: No se si esto es realmente necesario
+    #logger.debug(item.tostring())
     if item.infoLabels:
         return int(item.infoLabels.get('season', "1")), int(item.infoLabels.get('episode', "1"))
     else:
         temporada, capitulo = scrapertools.get_season_and_episode(item.title.lower()).split('x')
         return int(temporada), int(capitulo)
 
-
+'''
 def fichero_series(item):
     logger.info("pelisalacarta.channels.biblioteca fichero_series")
 
@@ -200,3 +230,4 @@ def limpiar_fichero(item):
 
     # eliminar huerfanos
     return library.clean_up_file(item)
+'''
