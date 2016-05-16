ca86344dc12bf4549c4884048fad52c8c42221c0
David <r_david_@hotmail.com>
2016-05-16 16:51:31 +0200

    Mejoras canal Pelisxporno

--- python/main-classic/channels/pelisxporno.py
+++ python/main-classic/channels/pelisxporno.py
@@ -12,14 +12,6 @@ from core import scrapertools
 from core import servertools
 from core.item import Item
 
-
-__channel__ = "pelisxporno"
-__category__ = "X"
-__type__ = "generic"
-__title__ = "Pelisxporno"
-__language__ = "ES"
-
-
 DEBUG = config.get_setting("debug")
     
 def isGeneric():
@@ -29,11 +21,23 @@ def mainlist(item):
     logger.info("[Pelisxporno.py] mainlist")
 
     itemlist = []
-    itemlist.append( Item(channel=__channel__, action="lista"  , title="Novedades" , url="http://www.pelisxporno.com/?order=date" ) )
-    itemlist.append( Item(channel=__channel__, action="categorias"  , title="Categorías" , url="http://www.pelisxporno.com" ) )
+    itemlist.append( Item(channel=item.channel, action="lista"  , title="Novedades" , url="http://www.pelisxporno.com/?order=date" ) )
+    itemlist.append( Item(channel=item.channel, action="categorias"  , title="Categorías" , url="http://www.pelisxporno.com" ) )
+    itemlist.append( Item(channel=item.channel, action="search"  , title="Buscar" , url="http://www.pelisxporno.com/?s=%s" ) )
 
     return itemlist
 
+def search(item,texto):
+    logger.info("[Pelisxporno.py] search:" + texto)
+    item.url = item.url % texto
+    try:
+        return lista(item) 
+    # Se captura la excepción, para no interrumpir al buscador global si un canal falla
+    except:
+        import sys
+        for line in sys.exc_info():
+            logger.error( "%s" % line )
+        return []
 
 def lista(item):
     logger.info("[Pelisxporno.py] lista")
@@ -56,18 +60,15 @@ def lista(item):
         #scrapedplot = match[0]  
         #if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
         #scrapedplot=strip_tags(scrapedplot)
-        itemlist.append( Item(channel=__channel__, action="detail", title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail , plot="" , folder=True) )
+        itemlist.append( Item(channel=item.channel, action="findvideos", title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail , plot="" , folder=True) )
+ 
  
     #Extrae la marca de siguiente página
-
-    patronvideos ='<a class="page larger" href="([^"]+)">([^"]+)<\/a>'
-    matches2 = re.compile(patronvideos,re.DOTALL).findall(data)
-    #scrapertools.printMatches(matches)
-    
- 
-    scrapedurl = matches2[0][0]
-    page = matches2[0][1]
-    itemlist.append( Item(channel=__channel__, action="lista", title="Página " + page , url=scrapedurl , plot="" , folder=True) )
+    next_page = re.compile('<a class="page larger" href="([^"]+)">([^"]+)<\/a>',re.DOTALL).findall(data)
+    if next_page:
+      scrapedurl = next_page[0][0]
+      page = next_page[0][1]
+      itemlist.append( Item(channel=item.channel, action="lista", title="Página " + page , url=scrapedurl , plot="" , folder=True) )
 
     return itemlist
 
@@ -80,26 +81,11 @@ def categorias(item):
 
     # Extrae las entradas (carpetas)
     bloque_cat = scrapertools.find_single_match(data, '<li id="categories-2"(.*?)</ul>')
-    logger.info("[Pelisxporno.py] "+ bloque_cat)
     patronvideos ='<a href="([^"]+)" >(.*?)</a>'
     matches = re.compile(patronvideos,re.DOTALL).findall(bloque_cat)
 
     for scrapedurl, scrapedtitle in matches:
         if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"]")
-        itemlist.append( Item(channel=__channel__, action="lista", title=scrapedtitle , url=scrapedurl , folder=True) )
-
-    return itemlist
-
-def detail(item):
-    logger.info("[Pelisxporno.py] detail")
-    itemlist = []
-
-    # Descarga la pagina
-    data = scrapertools.downloadpageGzip(item.url)
-
-    # Busca los enlaces a los videos de los servidores
-    video_itemlist = servertools.find_video_items(data=data)
-    for video_item in video_itemlist:
-        itemlist.append( Item(channel=__channel__ , action="play" , server=video_item.server, title=video_item.title, url=video_item.url, thumbnail=item.thumbnail, plot=item.plot, folder=False))
+        itemlist.append( Item(channel=item.channel, action="lista", title=scrapedtitle , url=scrapedurl , folder=True) )
 
     return itemlist
