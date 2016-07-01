b460d482a38e82539f0823ce04d06642aeebc765
SeiTaN <seitan80@gmail.com>
2016-07-01 14:46:07 +0200

    Update vseries.py

--- python/main-classic/channels/vseries.py
+++ python/main-classic/channels/vseries.py
@@ -28,8 +28,6 @@ __creationdate__ = "20140615"
 DEFAULT_HEADERS = []
 DEFAULT_HEADERS.append( ["User-Agent","Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; es-ES; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12"] )
 
-def isGeneric():
-    return True
 
 def mainlist(item):
     logger.info("pelisalacarta.channels.vseries mainlist")
