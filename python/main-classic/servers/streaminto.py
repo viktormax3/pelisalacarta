f2a908162c4c3231512b52572ff97b7b7aa81687
CmosGit <francm0785@gmail.com>
2016-12-13 18:49:32 +0100

    Corregido streaminto
    Gracias a robalo ;)

--- python/main-classic/servers/streaminto.py
+++ python/main-classic/servers/streaminto.py
@@ -29,7 +29,19 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     data = re.sub(r'\n|\t|\s+', '', scrapertools.cache_page(page_url))
 
     video_urls = []
-    media_url = scrapertools.get_match(data, """.setup\({file:"([^"]+)",image""")
+    try:
+        media_url = scrapertools.get_match(data, """.setup\({file:"([^"]+)",image""")
+    except:
+        js_data = scrapertools.find_single_match(data, "(eval.function.p,a,c,k,e.*?)</script>")
+        js_data = unPack(js_data)
+        media_url = scrapertools.get_match(js_data, """.setup\({file:"([^"]+)",image""")
+
+    if media_url.endswith("v.mp4"):
+        media_url_mp42flv = re.sub(r'/v.mp4$','/v.flv',media_url)
+        video_urls.append([scrapertools.get_filename_from_url(media_url_mp42flv)[-4:] + " [streaminto]", media_url_mp42flv])
+    if media_url.endswith("v.flv"):
+        media_url_flv2mp4 = re.sub(r'/v.flv$','/v.mp4',media_url)
+        video_urls.append([scrapertools.get_filename_from_url(media_url_flv2mp4)[-4:] + " [streaminto]", media_url_flv2mp4])        
     video_urls.append([scrapertools.get_filename_from_url(media_url)[-4:] + " [streaminto]", media_url])
 
     for video_url in video_urls:
@@ -87,3 +99,26 @@ def find_videos(data):
             logger.info("  url duplicada=" + url)
 
     return devuelve
+
+
+def unPack(packed):
+
+    pattern = "}\('(.*)', *(\d+), *(\d+), *'(.*)'\.split\('([^']+)'\)"
+    d = [ d for d in re.search(pattern, packed, re.DOTALL).groups() ]
+
+    p = d[0]; a = int(d[1]); c = int(d[2]); k = d[3].split(d[4])
+
+    if a <= 62: toString = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    else: toString = """ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+
+    def e(c):
+        return toString[c] if c < a else toString[c // a] + toString[c % a]
+
+    while c > 0:
+        c-= 1
+        if k[c]: x = e(c)
+        else: x = k[c]
+        y = k[c]
+        p = re.sub(r"(\b%s\b)" % x, y, p)
+
+    return p
