991d4b6da6031e5119fb08086201a986c0f22503
Palatosino <Palatosino@users.noreply.github.com>
2017-01-28 13:42:54 +0100

    Protect tmpPath

--- python/main-classic/platformcode/platformtools.py
+++ python/main-classic/platformcode/platformtools.py
@@ -971,9 +971,13 @@ def play_torrent(item, xlistitem, mediaurl):
         # Importamos el cliente
         from btserver import Client
 
+        clientTmpPath = config.get_setting("downloadpath")
+        if not clientTmpPath:
+            clientTmpPath = config.get_data_path()
+
         # Iniciamos el cliente:
         c = Client(url=mediaurl, is_playing_fnc=xbmc.Player().isPlaying, wait_time=None, timeout=10,
-                   temp_path=os.path.join(config.get_setting("downloadpath"), "torrent"), print_status=debug)
+                   temp_path=os.path.join(clientTmpPath, "torrent"), print_status=debug)
 
         # Mostramos el progreso
         progreso = dialog_progress("Pelisalacarta - Torrent", "Iniciando...")
