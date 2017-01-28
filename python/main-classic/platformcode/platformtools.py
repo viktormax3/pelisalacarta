75e5e1a4b451bb2f9ed9ed0de6fdbd411af6ebd3
Palatosino <Palatosino@users.noreply.github.com>
2017-01-28 19:14:52 +0100

    Cambiar el nombre del directorio
    SI damos la posiblidad de poner nuevos directorios, mejor que se indique que el directorio "torrent" es de pelisalacarta.
    Tal vez incluso podríamos quitar esto y que funcione directamente sobre el directorio que se indique en downloadpath, sin un subdirectorio, pero bueno, por ahora lo dejo con el subdirectorio.

--- python/main-classic/platformcode/platformtools.py
+++ python/main-classic/platformcode/platformtools.py
@@ -977,7 +977,7 @@ def play_torrent(item, xlistitem, mediaurl):
 
         # Iniciamos el cliente:
         c = Client(url=mediaurl, is_playing_fnc=xbmc.Player().isPlaying, wait_time=None, timeout=10,
-                   temp_path=os.path.join(clientTmpPath, "torrent"), print_status=debug)
+                   temp_path=os.path.join(clientTmpPath, "pelisalacarta-torrent"), print_status=debug)
 
         # Mostramos el progreso
         progreso = dialog_progress("Pelisalacarta - Torrent", "Iniciando...")
