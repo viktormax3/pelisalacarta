bb58aa14a958ed42ecdc0d7650399b5195fe4720
CmosGit <francm0785@gmail.com>
2017-02-21 01:24:08 +0100

    Nuevo canal y arreglos
    Nuevo canal datoporn y arreglado su conector y canal pelisxporno

--- python/main-classic/channels/pelisxporno.py
+++ python/main-classic/channels/pelisxporno.py
@@ -1,89 +1,70 @@
 # -*- coding: utf-8 -*-
-#------------------------------------------------------------
+# -----------------------------------------------------------
 # pelisalacarta - XBMC Plugin
 # Canal para Pelisxporno
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
-#------------------------------------------------------------
-import re
+# -----------------------------------------------------------
 
-from core import config
+from core import httptools
 from core import logger
 from core import scrapertools
-from core.item import Item
-
-DEBUG = config.get_setting("debug")
     
 
 def mainlist(item):
-    logger.info("[Pelisxporno.py] mainlist")
+    logger.info()
 
     itemlist = []
-    itemlist.append( Item(channel=item.channel, action="lista"  , title="Novedades" , url="http://www.pelisxporno.com/?order=date" ) )
-    itemlist.append( Item(channel=item.channel, action="categorias"  , title="Categorías" , url="http://www.pelisxporno.com" ) )
-    itemlist.append( Item(channel=item.channel, action="search"  , title="Buscar" , url="http://www.pelisxporno.com/?s=%s" ) )
+    itemlist.append(item.clone(action="lista", title="Novedades", url="http://www.pelisxporno.com/?order=date"))
+    itemlist.append(item.clone(action="categorias", title="Categorías", url="http://www.pelisxporno.com"))
+    itemlist.append(item.clone(action="search", title="Buscar", url="http://www.pelisxporno.com/?s=%s"))
 
     return itemlist
+
 
 def search(item,texto):
-    logger.info("[Pelisxporno.py] search:" + texto)
-    texto = texto.replace(" ", "+")
+    logger.info()
     item.url = item.url % texto
-    try:
-        return lista(item) 
-    # Se captura la excepción, para no interrumpir al buscador global si un canal falla
-    except:
-        import sys
-        for line in sys.exc_info():
-            logger.error( "%s" % line )
-        return []
+    return lista(item) 
+
 
 def lista(item):
-    logger.info("[Pelisxporno.py] lista")
+    logger.info()
     itemlist = []
 
     # Descarga la pagina  
-    data = scrapertools.downloadpageGzip(item.url)
+    data = httptools.downloadpage(item.url).data
 
     # Extrae las entradas (carpetas)
-                        
-    patronvideos ='<div class="thumb">\n.*?<a href="([^"]+)" title="([^"]+)">.*?<img src="([^"]+)".*?\/>'
-    matches = re.compile(patronvideos,re.DOTALL).findall(data)
+    patron = '<div class="thumb">.*?<a href="([^"]+)" title="([^"]+)">.*?<img src="([^"]+)".*?' \
+             '<div class="duration".*?>([^<]+)<'
+    matches = scrapertools.find_multiple_matches(data, patron)
+    for scrapedurl, scrapedtitle, scrapedthumbnail, duration in matches:
+        if duration:
+            scrapedtitle += "  (%s)" % duration
 
-    for match in matches:
-        scrapedtitle = match[1]
-        #scrapedtitle = scrapedtitle.replace("&#8211;","-")
-        #scrapedtitle = scrapedtitle.replace("&#8217;","'")
-        scrapedurl = match[0]
-        scrapedthumbnail = match[2]
-        #scrapedplot = match[0]  
-        #if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
-        #scrapedplot=strip_tags(scrapedplot)
-        itemlist.append( Item(channel=item.channel, action="findvideos", title=scrapedtitle , url=scrapedurl , thumbnail=scrapedthumbnail , plot="" , folder=True) )
- 
- 
+        itemlist.append(item.clone(action="findvideos", title=scrapedtitle, url=scrapedurl, thumbnail=scrapedthumbnail))
+
     #Extrae la marca de siguiente página
-    next_page = re.compile('<a class="page larger" href="([^"]+)">([^"]+)<\/a>',re.DOTALL).findall(data)
+    next_page = scrapertools.find_single_match(data, '<a class="page larger" href="([^"]+)">([^"]+)</a>')
     if next_page:
-      scrapedurl = next_page[0][0]
-      page = next_page[0][1]
-      itemlist.append( Item(channel=item.channel, action="lista", title="Página " + page , url=scrapedurl , plot="" , folder=True) )
+      scrapedurl = next_page[0]
+      page = next_page[1]
+      itemlist.append(item.clone(action="lista", title=">> Página Siguiente (%s)" % page, url=scrapedurl))
 
     return itemlist
 
+
 def categorias(item):
-    logger.info("[Pelisxporno.py] categorias")
+    logger.info()
     itemlist = []
 
     # Descarga la pagina  
-    data = scrapertools.downloadpageGzip(item.url)
+    data = httptools.downloadpage(item.url).data
 
     # Extrae las entradas (carpetas)
     bloque_cat = scrapertools.find_single_match(data, '<li id="categories-2"(.*?)</ul>')
-    patronvideos ='<a href="([^"]+)" >(.*?)</a>'
-    matches = re.compile(patronvideos,re.DOTALL).findall(bloque_cat)
-
+    matches = scrapertools.find_multiple_matches(bloque_cat, '<a href="([^"]+)" >(.*?)</a>')
     for scrapedurl, scrapedtitle in matches:
-        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"]")
-        itemlist.append( Item(channel=item.channel, action="lista", title=scrapedtitle , url=scrapedurl , folder=True) )
+        itemlist.append(item.clone(action="lista", title=scrapedtitle, url=scrapedurl))
 
     return itemlist
--- python/main-classic/servers/datoporn.py
+++ python/main-classic/servers/datoporn.py
@@ -7,6 +7,7 @@
 
 import re
 
+from core import httptools
 from core import logger
 from core import scrapertools
 from lib import jsunpack
@@ -15,10 +16,10 @@ from lib import jsunpack
 def test_video_exists(page_url):
     logger.info("(page_url='%s')" % page_url)
 
-    data = scrapertools.cache_page(page_url)
+    data = httptools.downloadpage(page_url).data
 
-    if 'FILE NOT FOUND' in data:
-        return False, "[Datoporn] El archivo no existe o ha sido borrado"
+    if 'File Not Found' in data or '404 Not Found' in data:
+        return False, "[Datoporn] El archivo no existe o ha sido borrado" 
 
     return True, ""
 
@@ -26,7 +27,7 @@ def test_video_exists(page_url):
 def get_video_url(page_url, premium=False, user="", password="", video_password=""):
     logger.info("url=" + page_url)
 
-    data = scrapertools.cache_page(page_url)
+    data = httptools.downloadpage(page_url).data
 
     media_urls = scrapertools.find_multiple_matches(data, 'file\:"([^"]+\.mp4)",label:"([^"]+)"')
     if not media_urls:
@@ -35,9 +36,20 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
         media_urls = scrapertools.find_multiple_matches(data, '\[\{file\:"([^"]+)"')
 
     # Extrae la URL
+    calidades = []
     video_urls = []
-    for media_url in sorted(media_urls, key=lambda x: int(x[1])):
-        video_urls.append(["." + media_url[0].rsplit('.', 1)[1] + " " + media_url[1] + "p [datoporn]", media_url[0]])
+    for media_url in sorted(media_urls, key= lambda x:int(x[1])):
+        calidades.append(int(media_url[1]))
+        try:
+            title = ".%s %sp [datoporn]" % (media_url[0].rsplit('.',1)[1], media_url[1])
+        except:
+            title = ".%s %sp [datoporn]" % (media_url[-4:], media_url[1])
+        video_urls.append([title, media_url[0]])
+
+    sorted(calidades)
+    m3u8 = scrapertools.find_single_match(data,'file\:"([^"]+\.m3u8)"')
+    if m3u8:
+        video_urls.insert(0, [".m3u8 %s [datoporn]" % calidades[-1], m3u8])
 
     for video_url in video_urls:
         logger.info("%s - %s" % (video_url[0], video_url[1]))
