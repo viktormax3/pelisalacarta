b9fe46280d50e1deeb237469a64541e3c2e705ed
Intel1 <luisriverap@hotmail.com>
2017-03-10 08:55:00 -0500

    Actualizado powvideo.py
    Actualizado powvideo.py por problemas de encoding del texto

--- python/main-classic/servers/powvideo.py
+++ python/main-classic/servers/powvideo.py
@@ -93,7 +93,7 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     return video_urls
 
 
-# Encuentra vÃ­deos del servidor en el texto pasado
+# Encuentra videos del servidor en el texto pasado
 def find_videos(data):
     encontrados = set()
     devuelve = []
