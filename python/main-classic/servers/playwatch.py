4ace1e16b67b9f13d7f8adece8838cf82360cd82
CmosGit <francm0785@gmail.com>
2017-03-21 17:56:28 +0100

    Arreglos y nuevos conectores
    Nuevos: vidgot, watchvideo, vidup, userscloud, playwatch
    Corregidos: Auroravid, bitvidsx, datoporn, divxstage, flashx,
    wholecloud, vidtome

--- python/main-classic/servers/playwatch.py
+++ python/main-classic/servers/playwatch.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------
 # pelisalacarta - XBMC Plugin
-# Conector para vidto.me
+# Conector para playwatch
 # http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
 # ------------------------------------------------------------
 
 import re
+import base64
 
 from core import httptools
 from core import logger
@@ -15,10 +16,10 @@ from core import scrapertools
 def test_video_exists(page_url):
     logger.info("(page_url='%s')" % page_url)
 
-    data = httptools.downloadpage(page_url).data
+    response = httptools.downloadpage(page_url, follow_redirects=False)
     
-    if "Not Found" in data or "File Does not Exist" in data:
-        return False, "[Vidto.me] El fichero no existe o ha sido borrado"
+    if not response.sucess or response.headers.get("location"):
+        return False, "[Playwatch] El fichero no existe o ha sido borrado"
 
     return True, ""
 
@@ -26,17 +27,13 @@ def test_video_exists(page_url):
 def get_video_url(page_url, premium=False, user="", password="", video_password=""):
     logger.info("url=" + page_url)
 
-    data = httptools.downloadpage(page_url).data
+    data = httptools.downloadpage(page_url, follow_redirects=False).data
 
-    # Extrae la URL
-    #{file:"http://188.240.220.186/drjhpzy4lqqwws4phv3twywfxej5nwmi4nhxlriivuopt2pul3o4bkge5hxa/video.mp4",label:"240p"}
-    video_urls = []
-    media_urls = scrapertools.find_multiple_matches(data, '\{file\s*:\s*"([^"]+)",label\s*:\s*"([^"]+)"\}')
-    for media_url, label in media_urls:
-        ext = scrapertools.get_filename_from_url(media_url)[-4:]
-        video_urls.append(["%s (%s) [vidto.me]" % (ext, label), media_url])
+    code = scrapertools.find_single_match(data, ' tracker:\s*"([^"]+)"')
+    media_url = base64.b64decode(code)
+    ext = scrapertools.get_filename_from_url(media_url)[-4:]
+    video_urls = [["%s  [playwatch]" % ext, media_url]]
 
-    video_urls.reverse()
     for video_url in video_urls:
         logger.info("%s - %s" % (video_url[0], video_url[1]))
 
@@ -50,18 +47,18 @@ def find_videos(data):
     encontrados = set()
     devuelve = []
 
-    #http://vidto.me/z3nnqbspjyne
-    #http://vidto.me/embed-z3nnqbspjyne
-    patronvideos = 'vidto.me/(?:embed-|)([A-z0-9]+)'
+    #http://playwatch.me/z3nnqbspjyne
+    #http://playwatch.me/embed/z3nnqbspjyne
+    patronvideos = 'playwatch.me/(?:embed/|)([A-z0-9]+)'
     logger.info("#" + patronvideos + "#")
     matches = re.compile(patronvideos, re.DOTALL).findall(data)
 
     for match in matches:
-        titulo = "[vidto.me]"
-        url = "http://vidto.me/embed-%s.html" % match
+        titulo = "[playwatch]"
+        url = "http://playwatch.me/embed/%s" % match
         if url not in encontrados:
             logger.info("  url=" + url)
-            devuelve.append([titulo, url, 'vidtome'])
+            devuelve.append([titulo, url, 'playwatch'])
             encontrados.add(url)
         else:
             logger.info("  url duplicada=" + url)
