e7061efa06ae1d76fbd11846eace518c85fac81e
CmosGit <francm0785@gmail.com>
2017-03-28 15:26:09 +0200

    Fix youwatch

--- python/main-classic/servers/youwatch.py
+++ python/main-classic/servers/youwatch.py
@@ -9,29 +9,35 @@ import re
 
 from core import logger
 from core import scrapertools
+from core import httptools
 
 
 def test_video_exists(page_url):
-    logger.info("pelisalacarta.servers.youwatch test_video_exists(page_url='%s')" % page_url)
-    data = scrapertools.cache_page(page_url)
-    if "File Not Found" in data: return False, "[Youwatch] El archivo no existe o ha sido borrado"
+    logger.info("(page_url='%s')" % page_url)
+    data = httptools.downloadpage(page_url).data
+    if "File Not Found" in data:
+        return False, "[Youwatch] El archivo no existe o ha sido borrado"
+    url_redirect = scrapertools.find_single_match(data, '<iframe src="([^"]+)"')
+    data = httptools.downloadpage(url_redirect).data
+    if "We're sorry, this video is no longer available" in data:
+        return False, "[Youwatch] El archivo no existe o ha sido borrado"
+
     return True, ""
 
 
 def get_video_url(page_url, premium=False, user="", password="", video_password=""):
-    logger.info("pelisalacarta.servers.youwatch get_video_url(page_url='%s')" % page_url)
+    logger.info("(page_url='%s')" % page_url)
 
-    redireccion = scrapertools.getLocationHeaderFromResponse(page_url)
-    data = scrapertools.cache_page(redireccion)
+    data = httptools.downloadpage(page_url).data
     url_redirect = scrapertools.find_single_match(data, '<iframe src="([^"]+)"')
-    data = scrapertools.cache_page(url_redirect)
+    data = httptools.downloadpage(url_redirect).data
 
     url = scrapertools.get_match(data, '{file:"([^"]+)"')
-    video_url = url + "|Referer=" + url_redirect
+    video_url = "%s|Referer=%s" % (url, url_redirect)
     video_urls = [[scrapertools.get_filename_from_url(url)[-4:] + " [youwatch]", video_url]]
 
     for video_url in video_urls:
-        logger.info("pelisalacarta.servers.youwatch %s - %s" % (video_url[0], video_url[1]))
+        logger.info("%s - %s" % (video_url[0], video_url[1]))
 
     return video_urls
 
@@ -44,12 +50,12 @@ def find_videos(data):
     # http://chouhaa.info/embed-ihc21y2tqpnt.html
     # http://youwatch.org/ihc21y2tqpnt
     patronvideos = 'http://(?:youwatch.org|chouhaa.info)/(?:embed-|)([a-z0-9]+)'
-    logger.info("pelisalacarta.servers.youwatch find_videos #" + patronvideos + "#")
+    logger.info("#" + patronvideos + "#")
     matches = re.compile(patronvideos, re.DOTALL).findall(data)
 
     for match in matches:
         titulo = "[youwatch]"
-        url = "http://youwatch.org/embed-%s.html" % match
+        url = "http://voodaith7e.com/embed-%s.html" % match
         if url not in encontrados:
             logger.info("  url=" + url)
             devuelve.append([titulo, url, 'youwatch'])
