4fe70579483a793c2c1f6489f277112f9877232e
SeiTaN <seitan80@gmail.com>
2017-04-05 21:52:29 +0200

    eliminado breadcrump de logger.info

--- python/main-classic/channels/miltorrents.py
+++ python/main-classic/channels/miltorrents.py
@@ -75,7 +75,7 @@ api_fankey ="dffe90fba4d02c199ae7a9e71330c987"
 
 
 def mainlist(item):
-    logger.info("pelisalacarta.miltorrents mainlist")
+    logger.info()
     check_bg = item.action
     
     if str(check_bg) == "":
@@ -99,7 +99,7 @@ def mainlist(item):
 
 
 def search(item,texto):
-    logger.info("pelisalacarta.miltorrent search")
+    logger.info()
     texto = texto.replace(" ","+")
     if item.extra:
      if item.extra.split("|")[0]== "series":
@@ -134,7 +134,7 @@ def search(item,texto):
               logger.error( "%s" % line )
               return []
 def peliculas(item):
-    logger.info("pelisalacarta.miltorrents peliculas")
+    logger.info()
     itemlist = []
     
     # Descarga la página
@@ -238,7 +238,7 @@ def peliculas(item):
 
 
 def fanart(item):
-    logger.info("pelisalacarta.moviesultimate fanart")
+    logger.info()
     itemlist = []
     
     data = httptools.downloadpage(item.url).data
@@ -792,7 +792,7 @@ def fanart(item):
 
     return itemlist
 def capitulos(item):
-    logger.info("pelisalacarta.miltorrent ver_capitulo")
+    logger.info()
     
     
     itemlist = []
@@ -815,7 +815,7 @@ def capitulos(item):
 
 
 def findvideos(item):
-    logger.info("pelisalacarta.miltorrent findvideos")
+    logger.info()
     
     itemlist = []
     data = httptools.downloadpage(item.url).data
@@ -1055,7 +1055,7 @@ def findvideos(item):
 
 
 def info(item):
-    logger.info("pelisalacarta.miltorrent info")
+    logger.info()
     itemlist = []
     url=item.url
     id = item.extra
@@ -1221,7 +1221,7 @@ def info(item):
     infoplus.start(item_info, peliculas)
 
 def info_capitulos(item):
-    logger.info("pelisalacarta.miltorrent info_capitulos")
+    logger.info()
     
     
     url= "https://api.themoviedb.org/3/tv/"+item.show.split("|")[5]+"/season/"+item.extra.split("|")[2]+"/episode/"+item.extra.split("|")[3]+"?api_key="+api_key+"&language=es"
@@ -1472,7 +1472,7 @@ def convert_size(size):
 
 
 def busqueda(item):
-    logger.info("pelisalacarta.channels.buscador search")
+    logger.info()
     cat = [item.extra.split("|")[0].replace("tv", "serie"), 'torrent']
     new_item = Item()
     new_item.extra = item.extra.split("|")[1].replace("+", " ")
