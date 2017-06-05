65816c206be494e7e889dbb70eecc811f2b48466
CmosGit <francm0785@gmail.com>
2017-06-05 16:17:39 +0200

    Corregidos algunos regex y conectores
    Reparado watchers
    Añadido server tusfiles.net a tusfiles
    Regex de auroravid, datoporn, netutv, rapidvideo
    Raptu y rapidvideo muestran aviso de que no funcionan por debajo de Kodi
    17

--- python/main-classic/servers/raptu.py
+++ python/main-classic/servers/raptu.py
@@ -14,9 +14,15 @@ from core import scrapertools
 
 def test_video_exists(page_url):
     logger.info("(page_url='%s')" % page_url)
-    data = httptools.downloadpage(page_url).data
+    try:
+        response = httptools.downloadpage(page_url)
+    except:
+        pass
 
-    if "Object not found" in data:
+    if not response.data or "urlopen error [Errno 1]" in str(response.code):
+        return False, "[Raptu] Este conector solo funciona a partir de Kodi 17"
+
+    if "Object not found" in response.data:
         return False, "[Raptu] El archivo no existe o ha sido borrado"
 
     return True, ""
