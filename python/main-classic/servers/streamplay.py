0aa780b362960549e011090a0eab3ccbd6f5d7e9
Intel1 <luisriverap@hotmail.com>
2017-06-14 08:20:13 -0500

    streamplay: Reparado por @r0balo
    Reparado por @r0balo

--- python/main-classic/servers/streamplay.py
+++ python/main-classic/servers/streamplay.py
@@ -94,7 +94,7 @@ def decrypt(h):
     for c in range(len(h)):
         sig += [ord(h[c])]
 
-    k = "streamplayembedz"
+    k = "streamplayembedc"
     sec = []
     for c in range(len(k)):
         sec += [ord(k[c])]
