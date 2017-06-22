cce9abce11e7c5166da5661cbe30fecfe03917e6
Intel1 <luisriverap@hotmail.com>
2017-06-22 12:13:52 -0500

    Update powvideo.py

--- python/main-classic/servers/powvideo.py
+++ python/main-classic/servers/powvideo.py
@@ -51,7 +51,8 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     data = httptools.downloadpage(page_url, post, headers=_cookie).data
 
     key = "".join(eval(scrapertools.find_single_match(data, '_[^=]+=(\[[^\]]+\]);'))[7:9])
-    key = key if not key.startswith("embed") else key[6:]+key[:6]
+    if key.startswith("embed"):
+        key = key[6:]+key[:6]
     matches = scrapertools.find_single_match(data, "<script type=[\"']text/javascript[\"']>(eval.*?)</script>")
     data = jsunpack.unpack(matches).replace("\\", "")
 
